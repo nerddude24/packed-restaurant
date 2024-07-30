2eeb5bda1314cc82a95e3fce107599f5c66b3534
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-07-30 14:36:50 +0100

    Add content to html for testing (to be removed)

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
 				test: /\.css$/i,
 				use: ["style-loader", "css-loader"],
 			},
+			{
+				test: /\.jpg$/i,
+				type: "asset/resource",
+			},
 		],
 	},
 	plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
