f40367895fbc8834637ba9590b7cfc1aff61c92f
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-07-30 13:35:45 +0100

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+};
