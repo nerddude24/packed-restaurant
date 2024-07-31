18b315076b78b8b9fdd2013fc1f527ff1cbd2e25
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-07-31 19:05:09 +0100

    Style the header

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ import { createElement, createMainElement } from "../util";
 
 export default function Home() {
 	const container = createMainElement();
-	const header = createElement("h1", "Packed Restaurant", ["header"]);
+	const header = createElement("h1", "Packed Restaurant", ["home-header"]);
 	const subHeader = createElement(
 		"p",
 		"A restaurant that combines a few tons of ingredients and spits them out to you in a plate!",
@@ -11,7 +11,7 @@ export default function Home() {
 
 	container.appendChild(header);
 	container.appendChild(subHeader);
-	
+
 	container.classList.add("flex-center");
 
 	return container;
